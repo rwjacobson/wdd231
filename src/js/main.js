92d01c1b67a24e798e186b000b17e8e7228ae574
rwjacobson <rwjacobson11@gmail.com>
2025-01-20 19:42:54 -0700

    started on part 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="name-park">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
+
+  
