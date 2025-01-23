b4302b23c7aaab503f045339a7adf0ff56d214bd
rwjacobson <rwjacobson11@gmail.com>
2025-01-23 14:16:05 -0700

    making it dynamic part 1

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
+// Update the link in the disclaimer area to read the name of the park and navigate to that park’s official site.
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
 disclaimer.innerHTML = parkData.fullName;
 
+// Update the name, designation, and states of the park in the hero.
 function parkInfoTemplate(info) {
     return `<a href="/" class="name-park">${info.name}</a>
     <p class="hero-banner__subtitle">
@@ -13,5 +15,13 @@ function parkInfoTemplate(info) {
       <span>${info.states}</span>
     </p>`;
   }
+const conent = document.getElementById("low-opacity-box");
+conent.innerHTML = parkInfoTemplate(parkData);
 
-  
+//Update the title of the page to read the name of the park.
+const title = document.querySelector(".name-park");
+title.innerHTML = parkData.name;
+
+// Use the first image in the list in the data for the hero image.
+const heroImg = document.querySelector(".park-img");
+heroImg.setAttribute("src", parkData.images[0].url);
